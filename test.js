e8417e4be92057dc2535978d052c3a7b048598c4
Jonathan Hulse <jayhulse95130@gmail.com>
2020-03-26 15:01:03 -0500

    First Commit

---
+++ test.js
@@ -0,0 +1,17 @@
+let animationName = ""
+let colorName = ""
+
+function storeAnimationName (name) {
+    animationName = name;
+    console.log(animationName , "This is the animation name") 
+}
+
+function storeColor (color) {
+    colorName = color;
+    console.log(colorName , "This is the color")
+}
+
+function createElement () {
+    let element = document.getElementById("elementContainer")
+    
+}
